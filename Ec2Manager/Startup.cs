79f5439a8a5f2f7b422961f55dd5aa34dc0e7849
Chad Roesler <ChadRoesler@Me.com>
2019-11-10 21:25:11 -0600

    Enabled Errorhandling and HTTPS
    Looks pretty now

--- Ec2Manager/Startup.cs
+++ Ec2Manager/Startup.cs
@@ -108,9 +108,8 @@ namespace Ec2Manager
             }
             else
             {
-                app.UseDeveloperExceptionPage();
-                //app.UseStatusCodePages();
-                //app.UseMiddleware<ErrorHandler>();
+                app.UseStatusCodePages();
+                app.UseMiddleware<ErrorHandler>();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
