bb08f4e3809349da4ad5b3fdea86cec1f97506a0
Chad Roesler <ChadRoesler@Me.com>
2019-11-25 13:03:56 -0600

    missed this as well

--- Ec2Manager/Constants/ErrorStrings.cs
+++ Ec2Manager/Constants/ErrorStrings.cs
@@ -4,7 +4,7 @@
     {
         internal const string StartEc2InstanceError = "Unable to Start Instance: {0}.  The following error occured: {1}";
         internal const string RebootEc2InstanceError = "Unable to Reboot Instance {0}.  The following error occured: {1}";
-        internal const string ErrorLoadingAccount = "Unable to load account: {0}.  The following error occured: {1}";
-        internal const string ErrorLoadingKmsSecret = "Unable to load Secret: {0}.  The following error occured: {1}";
+        internal const string ErrorLoadingAccount = "Unable to load Account: {0}.  The following error occured: {1}";
+        internal const string ErrorLoadingSecret = "Unable to load Secret: {0}.  The following error occured: {1}";
     }
 }
